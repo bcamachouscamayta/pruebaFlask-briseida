5fcdbe2654ece56c99271439e9b2480993d8e286
briseida camacho <evabri2008cue@gmail.com>
2025-03-31 12:05:24 -0300

    agregue ruta

--- app.py
+++ app.py
@@ -3,6 +3,9 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello():
+@app.route('/hola/chau')
+def hola():
+##al lado de / se pone otro nombre opcional 
     return 'Hello, World!'
+def chau():
+    return'chau'
