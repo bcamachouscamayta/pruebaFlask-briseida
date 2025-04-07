db022608ae1903940040803fbfe3d5a27d9d52a6
briseida camacho <evabri2008cue@gmail.com>
2025-04-07 12:01:53 -0300

    agregamos rutas estaticas

--- README.md
+++ README.md
@@ -17,21 +17,40 @@ para instalar la dependencia
 ```
 pip install flask
 ```
+
 ## tip para tener el historial de comando
 
 ```
 history
 ```
+
 ## para haceer la lista mas **negrita**
 
-## para correr el programa ##
+## para correr el programa
 
 para acceder solo en la maquina local:
+
 ```
-flask run 
+flask run
+```
+
+te reinicia automaticamaente la maquina para correrlo:
+
+```
+flask run -h 0.0.0.0 --debug
+
 ```
 
 para acceder desde cualquier maquina de la red:
+
 ```
 flask run -h 0.0.0.0
+```
+
+desde otra maquina,te reinicia automaticamente la pagina:
+´´´
+flask run -h 0.0.0.0 --debug
+
+```
+
 ```
--- app.py
+++ app.py
@@ -1,6 +1,23 @@
 from flask import Flask
+from flask import Flask, url_for
 
 app = Flask(__name__)
+
+@app.route("/")
+def main():
+    url_ekisde = url_for("ekisde")
+    url_ekisde = url_for("ekisde")
+    url_ekisde = url_for("ekisde")
+
+return f"""
+<a href="{url_ekisde}>lol</a>
+<br>
+<a href="{url_ekisde}>lol</a>
+<br>
+<a href="{url_ekisde}>lol</a>
+
+"""
+
 
 
 @app.route('/hola/chau')
@@ -9,3 +26,11 @@ def hola():
     return 'Hello, World!'
 def chau():
     return'chau'
+
+@app.route("/holaaa")
+def salir():
+    return "<p> chauuu</p>"
+
+@app.route("/saludar/por-nombre/<string:nombre>")
+def sxm(nombre):
+    return f"<p>hola {nombre}</p>"
